27ad08273ede241f7254b3fc07bb6116e0c5e22a
NicholasRHeinz <nickrheinz@hotmail.com>
2023-09-24 14:07:20 -0500

    Working velocity measurement with telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityEncoder.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import java.util.concurrent.TimeUnit;
+
 /** VelocityEncoder:
  * Calculates velocity from a given encoder
  *
@@ -12,9 +14,9 @@ public class VelocityEncoder {
 
     /* Local Variables */
     /** <h1>Last Time</h1>
-     * <b>Units:</b> milliseconds
+     * <b>Units:</b> microseconds
      */
-    private float lastTimeUpdate_fl;
+    private float lastMicroSecondTime_fl;
 
     /** <h1>Last Encoder Count</h1>
      *  <b>Units:</b> Encoder Counts
@@ -34,25 +36,129 @@ public class VelocityEncoder {
      */
     private float filteredVelocity;
 
+    private boolean velocitySynched;
+
+    /**
+     * <h1>Gear Ratio</h1>
+     * Multiplier between motor output speed and gearbox output speed
+     */
+    private float gearRatio;
+
+    /**
+     * <h1>Filtered Velocity Time Constant</h1>
+     * Value 0-1 of how much of the delta instant velocity to add to the filtered velocity
+     */
+    private float filterTimeConstant = (float)0.30;
+
     /**
      * <h1>Velocity Encoder</h1>
      * Helper object for calculating rev/sec from a standard encoder
-     * @param gearRatio Multiplier between motor output speed and gearbox output speed
+     * @param encoderGearRatio Multiplier between motor output speed and gearbox output speed
      */
-    public VelocityEncoder(float gearRatio) {
+    public VelocityEncoder(float encoderGearRatio) {
         /* Locals */
 
         /* Constants */
         final float INITIAL_TIME = 0;
         final int INITIAL_ENCODER_COUNT = 0;
         final int INITIAL_VELOCITY = 0;
+        final boolean INITIAL_SYNC = false;
 
         /* Reset parameters to initial values */
-        lastTimeUpdate_fl = INITIAL_TIME;
-        lastEncoderCount_int = INITIAL_ENCODER_COUNT;
-        instantVelocity = INITIAL_VELOCITY;
-        filteredVelocity = INITIAL_VELOCITY;
+        this.lastMicroSecondTime_fl = INITIAL_TIME;
+        this.lastEncoderCount_int = INITIAL_ENCODER_COUNT;
+        this.instantVelocity = INITIAL_VELOCITY;
+        this.filteredVelocity = INITIAL_VELOCITY;
+        this.velocitySynched = INITIAL_SYNC;
 
-        runtime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        this.gearRatio = encoderGearRatio;
+
+        this.runtime = new ElapsedTime();
+    }
+
+    public float Update(int encoderValue) {
+        /* Locals */
+        float currentMicroSecondTime_fl;
+        float microSecondDelta_fl;
+        int encoderDelta_int;
+        float motorShaftVelocity_fl;
+
+        /* Constants */
+        final int MICROSECONDS_PER_SECOND = 1000000;
+        final int MINIMUM_ENCODER_DELTA = 28;
+        final float ZERO_VELOCITY_TIMEOUT_MICROSECONDS = 400000;
+
+        /* Update time and encoder deltas */
+        currentMicroSecondTime_fl = this.runtime.now(TimeUnit.MICROSECONDS);
+        microSecondDelta_fl = currentMicroSecondTime_fl - this.lastMicroSecondTime_fl;
+        encoderDelta_int = encoderValue - this.lastEncoderCount_int;
+
+        /* This is likely the first time Update has been
+           called for this encoder
+         */
+        if (!this.velocitySynched)
+        {
+            /* Update timestamp and counts */
+            this.lastMicroSecondTime_fl = currentMicroSecondTime_fl;
+            this.lastEncoderCount_int = encoderValue;
+
+            /* Velocity can be calculated next time around */
+            this.velocitySynched = true;
+            return 0;
+        }
+
+        /* Check for zero speed timeout */
+        if (microSecondDelta_fl > ZERO_VELOCITY_TIMEOUT_MICROSECONDS)
+        {
+            this.instantVelocity = 0;
+            this.velocitySynched = false;
+            UpdateFilteredVelocity(this.instantVelocity);
+            return microSecondDelta_fl;
+        }
+
+        /* Check for minimum encoder counts */
+        if (Math.abs(encoderDelta_int) < MINIMUM_ENCODER_DELTA)
+        {
+            return 0;
+        }
+
+        /* Calculate rev/sec based on encoderDelta_int and microSecondDelta_fl  */
+        motorShaftVelocity_fl = ((float)encoderDelta_int / 28) / (microSecondDelta_fl / MICROSECONDS_PER_SECOND);
+        this.instantVelocity = motorShaftVelocity_fl / this.gearRatio;
+
+        /* Update last timestamp and encoder counts */
+        this.lastMicroSecondTime_fl = currentMicroSecondTime_fl;
+        this.lastEncoderCount_int = encoderValue;
+
+        UpdateFilteredVelocity(this.instantVelocity);
+
+        return microSecondDelta_fl;
+    }
+
+    private void UpdateFilteredVelocity(float instantVelocity)
+    {
+        /* Locals */
+
+        /* Constants */
+
+        if (instantVelocity == 0)
+        {
+            this.filteredVelocity = 0;
+        }
+
+        this.filteredVelocity = this.filteredVelocity + (this.filterTimeConstant *
+                (instantVelocity - this.filteredVelocity));
+    }
+
+    public float GetVelocity() {
+        return this.instantVelocity;
+    }
+
+    public float getFilteredVelocity() {
+        return filteredVelocity;
+    }
+
+    public void setFilterTimeConstant(float filterTimeConstant) {
+        this.filterTimeConstant = filterTimeConstant;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityOpMode.java
@@ -1,71 +1,69 @@
 package org.firstinspires.ftc.teamcode;
 
+ import com.acmerobotics.dashboard.FtcDashboard;
  import com.qualcomm.robotcore.eventloop.opmode.Disabled;
  import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
  import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  import com.qualcomm.robotcore.hardware.DcMotor;
  import com.qualcomm.robotcore.util.ElapsedTime;
  import com.qualcomm.robotcore.util.Range;
- 
- @TeleOp(name="VelocityOpMode", group="Linear OpMode")
+
+ import java.util.concurrent.TimeUnit;
+
+@TeleOp(name="VelocityOpMode", group="Linear OpMode")
  public class VelocityOpMode extends LinearOpMode {
  
      // Declare OpMode members.
      private ElapsedTime runtime = new ElapsedTime();
-     private DcMotor leftDrive = null;
-     private DcMotor rightDrive = null;
+     private DcMotor testMotor = null;
+
+     private double lastTimestamp;
+     private double currentTimestamp;
+
+     private VelocityEncoder testEncoder = new VelocityEncoder((float)13.7);
+
  
      @Override
      public void runOpMode() {
-         telemetry.addData("Status", "Initialized");
-         telemetry.update();
- 
-         // Initialize the hardware variables. Note that the strings used here as parameters
-         // to 'get' must correspond to the names assigned during the robot configuration
-         // step (using the FTC Robot Controller app on the phone).
-         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
- 
-         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-         leftDrive.setDirection(DcMotor.Direction.REVERSE);
-         rightDrive.setDirection(DcMotor.Direction.FORWARD);
+         FtcDashboard dashboard = FtcDashboard.getInstance();
+         telemetry = dashboard.getTelemetry();
+
+         testMotor  = hardwareMap.get(DcMotor.class, "117 rpm");
+         testMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
  
          // Wait for the game to start (driver presses PLAY)
          waitForStart();
          runtime.reset();
+
+         currentTimestamp = runtime.now(TimeUnit.MICROSECONDS);
+         lastTimestamp = currentTimestamp;
  
          // run until the end of the match (driver presses STOP)
          while (opModeIsActive()) {
+             currentTimestamp = runtime.now(TimeUnit.MICROSECONDS);
  
              // Setup a variable for each drive wheel to save power level for telemetry
-             double leftPower;
-             double rightPower;
- 
-             // Choose to drive using either Tank Mode, or POV Mode
-             // Comment out the method that's not used.  The default below is POV.
- 
-             // POV Mode uses left stick to go forward, and right stick to turn.
-             // - This uses basic math to combine motions and is easier to drive straight.
-             double drive = -gamepad1.left_stick_y;
-             double turn  =  gamepad1.right_stick_x;
-             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
- 
-             // Tank Mode uses one stick to control each wheel.
-             // - This requires no math, but it is hard to drive forward slowly and keep straight.
-             // leftPower  = -gamepad1.left_stick_y ;
-             // rightPower = -gamepad1.right_stick_y ;
+             double joystick_power;
+             joystick_power = -gamepad1.left_stick_y;
  
              // Send calculated power to wheels
-             leftDrive.setPower(leftPower);
-             rightDrive.setPower(rightPower);
+             testMotor.setPower(joystick_power);
+
+             float timeDelta = testEncoder.Update(testMotor.getCurrentPosition());
  
              // Show the elapsed game time and wheel power.
-             telemetry.addData("Status", "Run Time: " + runtime.toString());
-             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+//             telemetry.addData("Motors", "Power (%.2f)", joystick_power);
+             telemetry.addData("Execution Rate", currentTimestamp - lastTimestamp );
+             if (timeDelta > 0)
+             {
+                 telemetry.addData("Last Delta", timeDelta);
+             }
+             telemetry.addData("Velocity rev/sec", testEncoder.GetVelocity());
+             telemetry.addData("Velocity rpm", testEncoder.GetVelocity() * 60);
+             telemetry.addData("Filtered Velocity rpm", testEncoder.getFilteredVelocity() * 60);
              telemetry.update();
+
+             lastTimestamp = currentTimestamp;
          }
      }
  }
